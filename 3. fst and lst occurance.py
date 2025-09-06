7c1de648d07557e648d53070b446762acb009850
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-09-06 12:09:08 +0600

    ac: lower llimit bst

--- 3. fst and lst occurance.py
+++ 3. fst and lst occurance.py
@@ -43,12 +43,52 @@ def bst_index(num, target):
     return -1
 
 #%% lower limit
-# def 
+'''
+the smallest index that is >= target
+important to NOTICE:
+-> smallest
+-> greatr or equal
+
+## to ensure smallest
+- we will the return imidiately after finding a match 
+- we will store that pos as `ans` and try to minimise as long as we can
+- when we will reach our boundery we will return the `ans` variable
+
+## here
+- for this problem we will ignore `greater` 
+- and will only look for `exact` match
+
+'''
+def lower_limit_bst(num, target):
+    if not num: return -1
+
+    # optimisation -> as the array is sorted
+    if target<num[0] or target>num[-1]:
+        return -1
+
+    ans = -1 
+    '''
+    initial value of `ans` could have been ln(num), if we consider `greater` than
+    as the last index is obiously greater than the target.
+    but we need to minimise the `ans`.
+    '''
+    low, high = 0, len(num)
+    while low<=high:
+        mid = (low+high)//2
+        if num[mid]==target:
+            ans = mid
+            high = mid - 1
+        elif num[mid]>target:
+            high = mid - 1
+        elif num[mid]<target:
+            low = mid + 1
+    
+    return ans
 
 #%%
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        fst_idx = bst_index(nums, target)
+        fst_idx = lower_limit_bst(nums, target)
         if fst_idx==-1:
             return -1, -1
         
